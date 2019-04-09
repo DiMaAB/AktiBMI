b770a878a6105195a4d7cb05d5f5940e5df3a071
Dima <dimaabramov555@gmail.com>
2019-04-09 13:00:59 +0300

    Updates

---
+++ precache-manifest.dd32a19bab2f92548c4a6a0030fb24d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "45f3e5466f52a2d78ca1ed0a0d4e3ff8",
+    "url": "/AktiBM/static/media/start.45f3e546.png"
+  },
+  {
+    "revision": "f5cb0ec81399531ef510",
+    "url": "/AktiBM/static/js/runtime~main.b81bbd63.js"
+  },
+  {
+    "revision": "30c9983b3deba4f1490b",
+    "url": "/AktiBM/static/js/main.47db2be7.chunk.js"
+  },
+  {
+    "revision": "09431e1d314187933644",
+    "url": "/AktiBM/static/js/2.fe7fe9fd.chunk.js"
+  },
+  {
+    "revision": "30c9983b3deba4f1490b",
+    "url": "/AktiBM/static/css/main.56a76a74.chunk.css"
+  },
+  {
+    "revision": "df27612cca4fe5fbabc0ce3ece9fe12f",
+    "url": "/AktiBM/index.html"
+  }
+];
